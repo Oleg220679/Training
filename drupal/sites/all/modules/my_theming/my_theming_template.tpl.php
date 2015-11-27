fd5526a509449504403ea8cf4066e1aa9ff66647
Oleg220679 <oleg220679@ukr.net>
2015-11-27 18:04:52 +0200

    renew

---
+++ drupal/sites/all/modules/my_theming/my_theming_template.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*Theme template for my_theming_template.
+*/
+?>
+
+<h4><?php print $text_one; ?></h4>
+<p><b><i><?php print $text_two; ?></i></b></p>
---
+++ drupal1/sites/all/modules/my_theming/my_theming_template.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+*Theme template for my_theming_template.
+*/
+?>
+<h6 class='my_example_template'><?php print $text_one; ?></h6>
+<p><?php print $text_two; ?></p>
